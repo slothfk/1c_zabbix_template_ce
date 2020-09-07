3682267cbe13b35ff3779bc83542326ee126fd73
Alexey Y. Fedotov <fedotov@kaminsoft.ru>
2020-09-07 11:53:32 +0300

    CE: Поддержка Windows (#35)

--- etc/zabbix/scripts/1c_common_module.sh
+++ etc/zabbix/scripts/1c_common_module.sh
@@ -7,18 +7,22 @@
 # Email: fedotov@kaminsoft.ru
 #
 
+# Тип операционной системы GNU/Linux или MS Windows
+[[ "$(uname -s)" != "Linux" ]] && IS_WINDOWS=1
+
 # Имя сервера, используемое в кластере 1С Предприятия
-HOSTNAME=$(hostname -s)
+[[ -z ${IS_WINDOWS} ]] && HOSTNAME=$(hostname -s) || HOSTNAME=$(hostname)
 
 # Добавление пути к бинарным файлам 1С Предприятия
-PATH=${PATH}:$(ls -d /opt/1C/v8*/[xi]* | tail -n1)
+[[ -z ${IS_WINDOWS} ]] && PATH=${PATH}:$(ls -d /opt/1C/v8*/[xi]* | tail -n1) ||
+    PATH=${PATH}:$(ls -d /c/Program\ Files*/1cv8/8.* | tail -n1)/bin
 
 # Модуль менеджера задач
 TM_MODULE="1c_common_tm.sh"
-[[ -f ${0%/*}/${TM_MODULE} ]] && source ${0%/*}/${TM_MODULE} 2>/dev/null && TM_AVAILABLE=1
+[[ -f "${WORK_DIR}"/${TM_MODULE} ]] && source "${WORK_DIR}"/${TM_MODULE} 2>/dev/null && TM_AVAILABLE=1
 
 # Проверить инициализацию переменной TMPDIR
-[[ -z ${TMPDIR} ]] && export TMPDIR="/var/tmp"
+[[ -z ${TMPDIR} ]] && export TMPDIR="/tmp"
 
 # Файл списка кластеров
 CLSTR_CACHE="${TMPDIR}/1c_clusters_cache"
@@ -62,18 +66,20 @@ function execute_tasks {
 
 # Проверить наличие ring license и вернуть путь до ring
 function check_ring_license {
-    [[ ! -f /etc/1C/1CE/ring-commands.cfg ]] &&  error "Не установлена утилита ring!"
-    LIC_TOOL=$(grep license-tools /etc/1C/1CE/ring-commands.cfg | cut -d: -f2)
+    [[ -z ${IS_WINDOWS} ]] && RING_CONF="/etc/1C/1CE/ring-commands.cfg" ||
+         RING_CONF="/C/ProgramData/1C/1CE/ring-commands.cfg"
+    [[ ! -f ${RING_CONF} ]] &&  error "Не установлена утилита ring!"
+    LIC_TOOL=$(grep license-tools "${RING_CONF}" | sed -r 's/.+file: //; s/\\/\//g; s/^(.{1}):/\/\1/')
 
     [[ -z ${LIC_TOOL} ]] && error "Не установлена утилита license-tools!"
 
-    ls ${LIC_TOOL%\/*\/*}/*ring*/ring
+    ls "${LIC_TOOL%\/*\/*}"/*ring*/ring*
 }
 
 # Получить список имен установленных программных лицензий
 # в качестве параметра ${1} указать путь до ring
 function get_license_list {
-    ${1} license list --send-statistics false | sed -re 's/^([0-9\-]+).*/\1/'
+    "${1}" license list --send-statistics false | sed -re 's/^([0-9\-]+).*/\1/'
 }
 
 # Установить параметры взаимодействия с сервисом RAS
@@ -101,8 +107,8 @@ function push_clusters_list {
     # Сохранить список UUID кластеров во временный файл
     function push_clusters_uuid {
         CURR_CLSTR=$( timeout -s HUP ${RAS_PARAMS[timeout]} rac cluster list \
-            ${1%%:*}:${RAS_PARAMS[port]} | grep -Pe '^(cluster|name|port)' | \
-            perl -pe 's/[ "]//g; s/^name:(.*)$/\1\n/; s/^cluster:(.*)/\1,/; s/^port:(.*)/\1,/; s/\n//' | \
+            ${1%%:*}:${RAS_PARAMS[port]} | grep -Pe '^($|cluster|name|port)' | \
+            perl -pe "s/.*: /,/; s/(.+)\n/\1/;" | sed 's/^,//; s/"//g' | \
             grep -Pe ${1##*:} | perl -pe 's/(.*,)\d+,(.*)/\1\2/; s/\n/;/' )
 
         [[ -n ${CURR_CLSTR} ]] && echo ${1%%:*}:${CURR_CLSTR} >> ${CLSTR_CACHE}
@@ -110,7 +116,8 @@ function push_clusters_list {
 
     # Получим список менеджеров кластеров, в которых участвует данный сервер, следующего вида:
     #   <имя_сервера>:<номер_порта_0>[|<номер_порта_1>[|..<номер_порта_N>]]
-    RMNGR_LIST=( $(pgrep -ax rphost | \
+    RMNGR_LIST=( $( [[ -z ${IS_WINDOWS} ]] && pgrep -ax rphost ||
+        { wmic path win32_process where "caption like 'rphost%'" get CommandLine | grep rphost; } |
         sed -r 's/.*-regport ([^ ]+).*/\0|\1/; s/.*-reghost ([^ ]+).*\|/\1:/' | sort -u | \
         awk -F: '{ if ( clstr_list[$1]== "" ) { clstr_list[$1]=$2 } \
             else { clstr_list[$1]=clstr_list[$1]"|"$2 } } \
